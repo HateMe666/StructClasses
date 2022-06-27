9f9a7c17e6e19c8f14851da75907d4a3c1607620
HateMe666 <nikita.kuzmich.11@gmail.com>
2022-06-27 13:48:56 +0300

    Initial commit

---
+++ StructClasses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StructClasses
+//
+//  Created by Nikita Kuzmich on 27.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
